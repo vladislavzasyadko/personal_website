d11c633ccb09e8d068fc187e60a943b2834f35b5
vlad <gilbert.cook111@gmail.com>
2020-03-23 23:17:18 +0300

    adjusting mobile version

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <div className={c.contact}>
             <a name='contact'></a>
             <div className={c.contact_container}>
-                <div>E-mail</div>
+                <div style={{backgroundImage: `url(https://cdn2.iconfinder.com/data/icons/social-icons-color/512/gmail-512.png)`}}>E-mail</div>
                 <div>Facebook</div>
                 <div>Vk</div>
             </div>
