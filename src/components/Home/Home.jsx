61e200229b818c0567463a89823a5b991a343de9
vlad <gilbert.cook111@gmail.com>
2020-03-23 21:21:17 +0300

    netlify connected

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 
                 <div className={h.text_area}>
                     <div className={h.big_text}>Welcome to my website!</div>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur exercitationem velit blanditiis cupiditate molestias. Blanditiis dicta laborum tenetur quam nobis earum beatae quae qui repellendus, eum architecto consequuntur. Nihil, unde.</p>
+                    <p>Consequatur exercitationem velit blanditiis cupiditate molestias. Blanditiis dicta laborum tenetur quam nobis earum beatae quae qui repellendus, eum architecto consequuntur. Nihil, unde.</p>
                 </div>
             </div>
             <ul class={h.circles}>
