07d6398e35a22f7fbdd7918e8de6e2e869e3c8e2
vlad <gilbert.cook111@gmail.com>
2020-03-23 23:29:36 +0300

    works section is refactored final mobile screen adjustments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Works from './components/Works/Works';
--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -9,9 +9,12 @@ const Works = () => {
             <a name='works'></a>
             <div className={w.title}>My works</div>
             <div className={w.works_container}>
-                <WorkItem name='Organizer' backgroundImage='https://www.pikpng.com/pngl/m/53-538504_calendar-icons-grey-news-and-events-icon-clipart.png' Url='https://github.com/vladislavzasyadko/organizer-app-angular'/>
-                <WorkItem name='TODO List' backgroundImage='' Url='https://github.com/vladislavzasyadko/angular_todolist'/>
-                <WorkItem name='Social network' backgroundImage='https://miro.medium.com/fit/c/256/256/0*BRl-uL7N9LF-1hiD.png' 
+                <WorkItem name='Organizer' 
+                backgroundImage='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png' 
+                Url='https://github.com/vladislavzasyadko/organizer-app-angular'/>
+                <WorkItem name='TODO List' backgroundImage='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png' 
+                Url='https://github.com/vladislavzasyadko/angular_todolist'/>
+                <WorkItem name='Social network' backgroundImage='https://cdn.worldvectorlogo.com/logos/react.svg' 
                 Url='https://github.com/vladislavzasyadko/react-course-social-network'/>
             </div>
         </div>
