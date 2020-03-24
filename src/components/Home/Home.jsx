5c9f1be4482c4e7e0665743f41d7816ac53f9e0c
vlad <gilbert.cook111@gmail.com>
2020-03-24 14:34:54 +0300

    minor changes to home and header section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
     return (
         <div id='home' className={h.home}>
             <div>
-
                 <div className={h.text_area}>
                     <div className={h.big_text}>Welcome to my website!</div>
                     <p>Consequatur exercitationem velit blanditiis cupiditate molestias. Blanditiis dicta laborum tenetur quam nobis earum beatae quae qui repellendus, eum architecto consequuntur. Nihil, unde.</p>
